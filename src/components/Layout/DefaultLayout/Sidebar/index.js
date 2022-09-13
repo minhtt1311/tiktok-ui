222bf495ea8dcb2f12e127d1a9c862e5cda2e0d8
Minh <minhtt@gmail.com>
2022-09-13 10:23:17 +0700

    Ui Header

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -6,7 +6,7 @@ const cx = classNames.bind(styles)
 function Sidedbar() {
     return (
         <aside className={cx('wrapper')}>
-            <h2>Sidebar</h2>
+            <h2>Sidebar </h2>
         </aside>
     );
 }
